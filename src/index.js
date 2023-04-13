00e72a81bc646255665a2e32b6d620f0a33f9b67
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-13 09:33:16 +0300

    json format example

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 'use strict';
 
-setTimeout(function(){
-    console.log('message1');
+const user = {
+    name: 'Alex',
+    surname: 'Smith',
+    age: 23,
+    address: {
+        sity: 'London',
+        street: 'Square',
+        house: 12,
+    },
+    isSubscribe: true,
+    songs: ['Song1', 'song2', 'song3'],
+    friend: null,
+    gymAbonement: undefined,
+    sayHi: function(){
+        console.log('hi');
+    },
+}
 
-}, 1000);
+console.log(JSON.stringify(user));
+const str = JSON.stringify(user);
 
-console.log('message2');
-console.log('message3');
-
-
-setInterval(() => {
-    console.log('message4');
-
-}, 20000)
+console.log(JSON.parse(str));
