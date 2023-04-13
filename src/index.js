c12aa5db81fca10695b778f4c30270065b81d79b
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-13 10:41:33 +0300

    Add then chaining example

--- src/index.js
+++ src/index.js
@@ -6,25 +6,15 @@ const p = fetch('https://fakestoreapi.com/products');
 // console.dir(p);
 
 p.then((response) => {
-    console.log('success');
-    console.log(response); //response - обьект ответа
-
-    const jsondata = response.json();
-    jsondata.then(
-        (jsData) => {
-            console.log('i am here')
-            console.log(jsData);
-        }, 
-        
-        () => {
-            console.log('ERROR')
-        }
-    );
-
-}, (error) => {
-    console.log('error :')
-    console.log(error);
-}); //говорим что делать, когда промис изменит статус
+    return response.json();
+    }) 
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log('error :')
+        console.log(error);
+    });
 
 
 
