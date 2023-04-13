0c5b84d728f5cf194c5884c17c9f93754618f2a7
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-13 09:05:06 +0300

    Async intro

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+setTimeout(function(){
+    console.log('message1');
+
+}, 1000);
+
+console.log('message2');
+console.log('message3');
+
+
+setInterval(() => {
+    console.log('message4');
+
+}, 20000)
