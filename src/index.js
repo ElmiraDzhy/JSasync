20ef37903b3f15d21c3ea1b912fdd33b0a2d1c8e
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-14 09:09:57 +0300

    Add some fix

--- src/index.js
+++ src/index.js
@@ -1,45 +1,42 @@
 'use strict';
 
 
-const products = fetch('https://fakestoreapi.com/products');
-
-// console.dir(p);
-
-products.then((response) => {
-    return response.json();
-    }) 
-    .then((data) => {
-        console.log(data);
-        const cardArray = data.map((obj) => createCard(obj));
-        console.log(cardArray);
-        const root = document.querySelector('#root');
-        root.append(...cardArray);
-    })
-    .catch((error) => {
-        console.log('error :')
-        console.log(error);
-    });
 
 
 
-const userData = fetch('https://randomuser.me/api/?results=5');
+const userData = fetch('https://randomuser.me/api/?results=20&seed=foobar');
 
 userData.then((responce) => responce.json())
-    .then((jsData) => console.log(jsData.results))
-    .catch(error => console.log(`This is error: ${error}`))
+    .then((jsData) => {
+        console.log(jsData.results)
+        const usersArr =  jsData.results.map((user) => createCard(user));
+        document.getElementById('root').append(...usersArr);
+    })
+    .catch(error => console.log(`Error: ${error}`))
     .finally(() => console.log('finally!!!!'));
 
 
 
 function createCard(obj){
+    const birth = obj.dob.date;
+    const date = new Date(birth);
 
-    const img = createElem('img', {classNames: ['card-image'], attributes: [{src: obj.image}]});
-    const h3 = createElem('h3', {classNames: ['card-title']}, obj.title);
-    const p = createElem('p', {classNames: ['card-description']}, obj.description);
-    const button = createElem('button', {classNames: ['card-price']}, 'Buy: ', obj.price, ' $');
-    const article = createElem('article', {classNames: ['card-container']}, img, h3, p, button);
+    const img = createElem('img', {classNames: ['user-image'], attributes: [{src: obj.picture.large}]});
+    const h3 = createElem('h3', {classNames: ['user-name']}, `${obj.name.title} ${obj.name.first} ${obj.name.last}`);
+    const p = createElem('p', {classNames: ['user-birth']}, `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`);
+    const gender = createElem('p', {classNames: ['user-gender']}, obj.gender);
+    const userMail = createElem('h4', {classNames: ['user-mail']}, obj.email);
+    const followBtn = createElem('button', {classNames: ['btn', 'follow']}, 'Follow me');
+    const textBtn = createElem('button', {classNames: ['btn', 'text']}, 'Text me');
 
-    console.log(obj.image);
+    const div = createElem('div', {classNames: ['div-wrapper']});
+    div.append(followBtn, textBtn);
+
+    const divBirth = createElem('div', {classNames: ['div-birth']});
+    divBirth.append(p, gender);
+
+
+    const article = createElem('article', {classNames: ['user-card']}, img, h3, divBirth, userMail, div);
 
     return article;
 }
@@ -49,6 +46,7 @@ function createElem(type, {classNames, attributes}, ...children){
 
 
     const elem = document.createElement(type);
+
     if(attributes){
         attributes.forEach((obj) => {
             for (const key in obj) {
@@ -58,6 +56,7 @@ function createElem(type, {classNames, attributes}, ...children){
        
     }
     elem.classList.add(...classNames);
+
     elem.append(...children);
 
 
