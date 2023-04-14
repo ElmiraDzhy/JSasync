3d44657d378e078297a2ee84a38ce7ee45e5bfd3
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-14 09:37:39 +0300

    Shrodinger cat

--- src/index.js
+++ src/index.js
@@ -2,63 +2,27 @@
 
 
 
+const p = new Promise(executor);
 
 
-const userData = fetch('https://randomuser.me/api/?results=20&seed=foobar');
+function executor(res, rej){
 
-userData.then((responce) => responce.json())
-    .then((jsData) => {
-        console.log(jsData.results)
-        const usersArr =  jsData.results.map((user) => createCard(user));
-        document.getElementById('root').append(...usersArr);
-    })
-    .catch(error => console.log(`Error: ${error}`))
-    .finally(() => console.log('finally!!!!'));
-
-
-
-function createCard(obj){
-    const birth = obj.dob.date;
-    const date = new Date(birth);
-
-    const img = createElem('img', {classNames: ['user-image'], attributes: [{src: obj.picture.large}]});
-    const h3 = createElem('h3', {classNames: ['user-name']}, `${obj.name.title} ${obj.name.first} ${obj.name.last}`);
-    const p = createElem('p', {classNames: ['user-birth']}, `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`);
-    const gender = createElem('p', {classNames: ['user-gender']}, obj.gender);
-    const userMail = createElem('h4', {classNames: ['user-mail']}, obj.email);
-    const followBtn = createElem('button', {classNames: ['btn', 'follow']}, 'Follow me');
-    const textBtn = createElem('button', {classNames: ['btn', 'text']}, 'Text me');
-
-    const div = createElem('div', {classNames: ['div-wrapper']});
-    div.append(followBtn, textBtn);
-
-    const divBirth = createElem('div', {classNames: ['div-birth']});
-    divBirth.append(p, gender);
-
-
-    const article = createElem('article', {classNames: ['user-card']}, img, h3, divBirth, userMail, div);
-
-    return article;
-}
-
-
-function createElem(type, {classNames, attributes}, ...children){
-
-
-    const elem = document.createElement(type);
-
-    if(attributes){
-        attributes.forEach((obj) => {
-            for (const key in obj) {
-                elem.setAttribute(key, obj[key]);
-            }
-        })
-       
+    if(Math.random() > 0.5){
+        res(true);
     }
-    elem.classList.add(...classNames);
 
-    elem.append(...children);
-
-
-    return elem;
+    else{
+        rej(false);
+    }
 }
+
+
+p.then(( ) => {
+    console.log('кіт живий');
+},
+() => {
+    console.log(`кіт не живий`);
+})
+
+
+console.log(p)
