ba09e352c553e922d2c8590b73a2ea3ed16524c0
ElyaDzhy <elmira.freshcode@gmail.com>
2023-04-14 10:47:42 +0300

    Add loadImg function

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 'use strict';
 
 
+// async load img
 
-async function getServerData(){
-    try{
-        const p = await fetch('https://randomuser.me/api/'); //p - response
 
-        const data = await p.json();
-        return data;
-    }catch(errs){
-        throw new Error();
-    }
-    
 
-    
+ function loadImg(src){
+    const img = document.createElement('img');
+    img.setAttribute('src', src);
 
+    return new Promise((res, rej) => {
+        img.addEventListener('load', (event) => {
+            res(img);
+        });
+        
+        img.addEventListener('error', () => {
+            rej('img error')
+        });
+
+    })
 }
 
 
+const root = document.querySelector('#root');
+const div = document.createElement('div');
+div.classList.add('wrapper');
+root.append(div);
 
-console.log(getServerData());
+const img = loadImg('https://i.pinimg./originals/e0/e9/f7/e0e9f7ec88289f2445827a47e7a0af79.jpg')
+    .then( (img) => div.append(img),
+            (err) => console.log(err)
+        );
 
